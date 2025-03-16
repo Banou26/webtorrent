4b5645931c336fd2cd66a023019f43d35b655c8d
Banou <me@banou.dev>
2025-03-16 23:47:12 +0100

    deps: ⬆️use webvpn, fkn deps & webtorrent forks

--- scripts/browser.webpack.js
+++ scripts/browser.webpack.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack'
 import TerserPlugin from 'terser-webpack-plugin'
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin'
 import info from '../package.json' assert { type: 'json' }
 
 /** @type {import('webpack').Configuration} */
@@ -27,11 +28,13 @@ export default {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      process: '/polyfills/process-fast.js'
+      process: '/polyfills/process-fast.js',
+      Buffer: ['buffer', 'Buffer']
     }),
     new webpack.DefinePlugin({
       global: 'globalThis'
-    })
+    }),
+    new NodePolyfillPlugin()
   ],
   optimization: {
     minimize: true,
